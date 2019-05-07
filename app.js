21475f352ef9a8d0feb057ad78a37d2c8caf173a
sivasankar21 <sankarsivapotnuru@gmail.com>
2019-05-07 23:28:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/getEmployees', authRouter);
 
 // catch 404 and forward to error handler
